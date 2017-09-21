9722db123f4408ba204d71fdde52ba269b03a3df
Leandro Ungari <leandroungari@gmail.com>
2017-09-21 16:47:10 -0300

    Arrumando * e .

--- ProjetoCompilador/test/testes/Analise.java
+++ ProjetoCompilador/test/testes/Analise.java
@@ -22,7 +22,7 @@ public class Analise {
         Arquivo arq = new Arquivo("correto1.txt");
 
         AnalisadorLexico a = new AnalisadorLexico();
-        //a.gerarLexico();
+        a.gerarLexico();
         
         /*
         for (String s : arq.getLinhas()) {
@@ -31,9 +31,9 @@ public class Analise {
             a.analisar(s, arq.getLinhas().indexOf(s));
             
         }*/
-        System.out.println(arq.getTexto());
-        a.analisar(arq.getTexto(), 0);
+        //System.out.println(arq.getTexto());
+        //a.analisar(arq.getTexto(), 0);
 
-        System.out.println(a.getTabela());
+        //System.out.println(a.getTabela());
     }
 }
