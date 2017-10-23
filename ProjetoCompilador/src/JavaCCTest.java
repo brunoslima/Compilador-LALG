0fdcba4a6ec1449f0825d4212c9ac076519986c2
Leandro Ungari <leandroungari@gmail.com>
2017-10-23 16:23:28 -0200

    JavaCC

---
+++ ProjetoCompilador/src/JavaCCTest.java
@@ -0,0 +1,55 @@
+
+import arquivo.Arquivo;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author leandroungari
+ */
+public class JavaCCTest {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String sourcecode = "novo.txt";
+
+        Arquivo arq = new Arquivo(new File(sourcecode));
+        String texto = arq.getTexto();
+
+        Grammar g = new Grammar(new StringReader(texto));
+
+        for (Token token : tokenize(g)) {
+            String name = GrammarConstants.tokenImage[token.kind];
+            System.out.println(token.beginLine + ":" + name + " => " + token.image);
+        }
+
+        ////////////////Sintática
+        try {
+            Grammar.ReInit(new StringReader(texto));
+            Grammar.compilationUnit();
+        } catch (ParseException ex) {
+            Logger.getLogger(JavaCCTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static List<Token> tokenize(Grammar parser) throws FileNotFoundException {
+        List<Token> tokens = new ArrayList<>();
+
+        Token token = Grammar.getNextToken();
+        while (token.kind != GrammarConstants.EOF) {
+            tokens.add(token);
+            token = Grammar.getNextToken();
+        }
+        return tokens;
+    }
+}
--- ProjetoCompilador/test/testes/Sintatico.java
+++ ProjetoCompilador/test/testes/Sintatico.java
@@ -4,29 +4,20 @@ package testes;
 import arquivo.Arquivo;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.StringReader;
-import sintatico.Lexer;
-import sintatico.Parser;
 
 public class Sintatico {
 
     public static void main(String[] args) throws FileNotFoundException {
 
 
-        String sourcecode = "teste.txt";
+        String sourcecode = "novo.txt";
 
         Arquivo arq = new Arquivo(new File(sourcecode));
         String texto = arq.getTexto();
         
-        try {
-            Parser p = new Parser(new Lexer(new StringReader(texto)));
-            String result = String.valueOf( p.parse());
-            System.out.println(result);
-            
-            System.out.println("Compilacao concluida com sucesso...");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        
+     
 
     }
 }
